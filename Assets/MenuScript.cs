767c81f0380a4974693a65475f3f7cc3a58c24fc
PronCan <twojini98@gmail.com>
2019-11-14 19:01:21 +0900

    add git ignore

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // 메뉴이동
+    // 게임시작
+    public void StartGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+    // 게임방법
+    // 종료
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
